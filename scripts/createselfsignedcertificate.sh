7989632f9e026740d65efb6ff76c64f8de73ff58
jdiekhoff <>
2023-07-25 01:02:58 +0000

    update cert scripts.

--- scripts/createselfsignedcertificate.sh
+++ scripts/createselfsignedcertificate.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+CERT_NAME=$1
 parent_path=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 pushd "$parent_path"
 
 mkdir ../certs
 
-openssl req -new -sha256 -nodes -out ../certs/server.csr -newkey rsa:2048 -keyout ../certs/server.key -config <( cat ../config/certs/server.csr.cnf )
+openssl req -new -sha256 -nodes -out ../certs/$CERT_NAME.csr -newkey rsa:2048 -keyout ../certs/$CERT_NAME.key -config <( cat ../config/certs/$CERT_NAME/server.csr.cnf )
 
-openssl x509 -req -in ../certs/server.csr -CA ~/ssl/rootCA.pem -CAkey ~/ssl/rootCA.key -CAcreateserial -out ../certs/server.crt -days 500 -sha256 -extfile ../config/certs/v3.ext
+openssl x509 -req -in ../certs/$CERT_NAME.csr -CA ~/ssl/rootCA.pem -CAkey ~/ssl/rootCA.key -CAcreateserial -out ../certs/$CERT_NAME.crt -days 500 -sha256 -extfile ../config/certs/$CERT_NAME/v3.ext
 
 popd
